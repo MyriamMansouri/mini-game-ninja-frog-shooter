e7e6cd5f5cba66d0d54f4668cdb1ae230c22f5cb
myriam mansouri <myriam.mansouri@gmail.com>
2020-06-30 13:17:34 -0400

    changed size of player

--- js/data.js
+++ js/data.js
@@ -8,13 +8,13 @@
 
 // These constants represent the width and height of an enemy in pixels
 // as well as the maximum number of enemies on screen at any given time.
-const ENEMY_WIDTH = 108;
-const ENEMY_HEIGHT = 90;
+const ENEMY_WIDTH = 72;
+const ENEMY_HEIGHT = 60;
 const MAX_ENEMIES = 3;
 
 // These constants represent the player width and height.
-const PLAYER_WIDTH = 96;
-const PLAYER_HEIGHT = 96;
+const PLAYER_WIDTH = 64;
+const PLAYER_HEIGHT = 64;
 
 // These constants represent the bonus items width and height.
 const BONUS_WIDTH = 64;
