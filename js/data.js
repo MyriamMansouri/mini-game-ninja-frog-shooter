55b544a56e78709a8f6cf00b8c2d00178b552a42
myriam mansouri <myriam.mansouri@gmail.com>
2020-06-30 13:47:39 -0400

    force portrait orientation on mobiles

--- js/data.js
+++ js/data.js
@@ -21,8 +21,10 @@ const BONUS_WIDTH = 64;
 const BONUS_HEIGHT = 64;
 const MAX_BONUS = 2;
 
+
+const MAX_LIFES = 3
+
 const GAME_WIDTH =
   Math.floor(document.body.clientWidth / PLAYER_WIDTH) * PLAYER_WIDTH;
 const GAME_HEIGHT = document.body.clientHeight;
 
-const MAX_LIFES = 3
